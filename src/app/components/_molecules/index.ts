ea5541131aacebed62538f5ea912551c353ddc0a
SabaTsaava017 <sabacaava7@gmail.com>
2024-02-07 01:33:53 +0400

    index.ts

---
+++ src/app/components/_molecules/index.ts
@@ -0,0 +1,7 @@
+"use client"
+export { default as FooterComponent } from "./Footer";
+export { default as HeaderComponent } from "./Header";
+export { default as SigninCard } from "./SigninCard";
+export { default as AddPostComponent } from "./feeds/addPost";
+export { default as PostCard } from "./feeds/PostCard";
+export {default as FeedsItem} from './feeds/feedsItem'
