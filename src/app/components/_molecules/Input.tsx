0c78e970c4a1385e82f390edf4d6899d5163f849
SabaTsaava017 <sabacaava7@gmail.com>
2024-02-07 01:34:08 +0400

    input.tsx

---
+++ src/app/components/_molecules/Input.tsx
@@ -0,0 +1,7 @@
+import { Message } from "@/app/styles/chat-styled/styled"
+
+export const ChatBox = () => {
+    return(
+        <Message type="text" placeholder="Write your message" ></Message>
+    )
+}
