c964a24ebd40d91ac3561a1d9294727a0c978d91
SabaTsaava017 <sabacaava7@gmail.com>
2024-02-08 01:00:58 +0400

    feed.tsx

---
+++ src/app/styles/Feed/Feed.tsx
@@ -0,0 +1,391 @@
+import { styled } from "styled-components";
+
+export const Container = styled.section`
+  background-color: #f7f9fb;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  overflow: scroll;
+`;
+export const New_Posts_Div = styled.div`
+  margin-top: 30px;
+  margin-left: 30px;
+  width: 850px;
+  border: solid white;
+  background-color: white;
+  border-radius: 10px;
+`;
+export const New_Post = styled.p`
+  margin-left: 30px;
+  margin-top: 15px;
+  font-size: 12px;
+`;
+export const Input = styled.input`
+  border: none;
+  color: gray;
+  margin-left: 30px;
+  font-size: 16px;
+  width: 100px;
+`;
+export const H2 = styled.h2`
+  font-size: 15px;
+  padding-left: 360px;
+  padding-top: 30px;
+  display: flex;
+`;
+export const Trending = styled.p`
+  color: #0275b1;
+`;
+export const Section2 = styled.div``;
+export const ProfileName = styled.h4``;
+
+export const ProfileInfo = styled.p`
+  margin-top: 10px;
+  padding-left: 20px;
+  font-size: 15px;
+  color: #181818;
+`;
+export const ProfileDiv = styled.div`
+  margin-left: 30px;
+  width: 850px;
+  height: 325px;
+  border: solid white;
+  background-color: white;
+  margin-top: 25px;
+  border-radius: 10px;
+`;
+export const ReadMore = styled.p`
+  color: #0275b1;
+  padding-top: 20px;
+  padding-left: 20px;
+`;
+export const HR = styled.hr`
+  width: 780px;
+  margin-left: 30px;
+  margin-top: 20px;
+  color: #f4f4f4;
+`;
+export const LikedThis = styled.p`
+  padding-top: 20px;
+  padding-left: 20px;
+  font-size: 10px;
+  display: flex;
+`;
+export const HR2 = styled.hr`
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+export const TheresaSteward = styled.h4`
+  font-size: 14px;
+  padding-left: 10px;
+  padding-top: 22px;
+`;
+export const IosDeveloper = styled.p`
+  font-size: 10px;
+  padding-left: 10px;
+`;
+export const HR3 = styled.hr`
+  margin-top: 30px;
+`;
+export const HR8 = styled.hr`
+  margin-top: 30px;
+  margin-right: 70px;
+`;
+
+export const LikeAndComment = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`;
+export const Like = styled.p`
+  margin-left: 15px;
+  margin-top: 10px;
+`;
+export const Comment = styled.p`
+  margin-left: 15px;
+  margin-top: 5px;
+`;
+export const Share = styled.p`
+  padding-left: 20px;
+  margin-top: 5px;
+`;
+export const Share2 = styled.p`
+  margin-right: 50px;
+  margin-top: 5px;
+`;
+export const ProfileImg = styled.img`
+  width: 52px;
+  border-radius: 50%;
+  margin-left: 20px;
+  margin-top: 15px;
+`;
+
+export const PictureName = styled.div`
+  display: flex;
+`;
+export const NewPostIcon = styled.img`
+  width: 20px;
+  margin-left: 10px;
+`;
+export const ForNameColor = styled.p`
+  color: #0275b1;
+`;
+export const LikeCommIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  object-fit: cover;
+`;
+export const LikeIconAndCount = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+export const ShareIcon = styled.img`
+  width: 30px;
+  margin-left: 600px;
+`;
+export const ShareIcon2 = styled.img`
+  width: 30px;
+  margin-left: 550px;
+`;
+export const Answer = styled.p`
+  margin-left: 30px;
+  padding-top: 15px;
+`;
+export const Pricture = styled.img`
+  width: 790px;
+  height: 300px;
+  margin-left: 25px;
+  margin-top: 30px;
+`;
+export const FileIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-top: 20px;
+  margin-left: 30px;
+`;
+export const DownloadIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-left: 200px;
+  margin-top: 20px;
+`;
+export const DownloadIcon2 = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-left: 240px;
+  margin-top: 20px;
+`;
+export const FileDiv = styled.div`
+  display: flex;
+  border: solid 1px #e9f0f8;
+  background-color: #e9f0f8;
+  margin-top: 30px;
+  width: 690px;
+  height: 80px;
+  border-radius: 5px;
+`;
+export const FileDivInfo = styled.p`
+  margin-top: 25px;
+  margin-left: 20px;
+`;
+export const InfoDiv = styled.div`
+  display: block;
+`;
+export const FileDivInfo2 = styled.p`
+  font-size: 10px;
+  margin-left: 20px;
+`;
+export const GuidelinesForiOS = styled.p`
+  margin-top: 40px;
+`;
+export const ProfileDiv2 = styled.div`
+  margin-left: 30px;
+  width: 850px;
+  height: 605px;
+  border: solid white;
+  background-color: white;
+  margin-top: 25px;
+  border-radius: 10px;
+`;
+export const ProfileDiv3 = styled.div`
+  margin-left: 30px;
+  width: 850px;
+  height: 505px;
+  border: solid white;
+  background-color: white;
+  margin-top: 25px;
+  border-radius: 10px;
+`;
+export const BacgroundImige = styled.div`
+  width: 290px;
+  height: 120px;
+  margin-left: 150px;
+  margin-top: 30px;
+  position: relative;
+`;
+export const ProfileImiges = styled.img`
+  width: 90px;
+  border-radius: 50px;
+  margin-left: 250px;
+  margin-top: 100px;
+  position: absolute;
+  top: 30%;
+  left: -35%;
+  transform: translate(-50%, -50%);
+`;
+export const ProfileName2 = styled.h6`
+  margin-left: 250px;
+  font-size: 14px;
+  padding-top: 10px;
+  margin-top: 60px;
+`;
+export const ProfileInfo2 = styled.p`
+  width: 240px;
+  margin-left: 170px;
+  margin-top: 30px;
+`;
+export const ButtonDiv = styled.div`
+  width: 290px;
+  height: 75px;
+  background-color: white;
+  margin-left: 150px;
+  margin-top: 30px;
+  border-radius: 5px;
+`;
+export const Button = styled.button`
+  background-color: #0077b5;
+  color: white;
+  width: 230px;
+  height: 32px;
+  font-size: 17px;
+  border: #0077b5;
+  margin-left: 30px;
+  margin-top: 20px;
+  border-radius: 5px;
+`;
+export const GroupDiv = styled.div`
+  border: solid white;
+  width: 290px;
+  height: 400px;
+  margin-left: 150px;
+  margin-top: 40px;
+  background-color: white;
+  border-radius: 5px;
+`;
+export const GroupAndList = styled.div`
+  display: flex;
+`;
+export const Mygroup = styled.p`
+  font-size: 12px;
+  padding-left: 20px;
+  padding-top: 20px;
+`;
+export const EditList = styled.p`
+  font-size: 12px;
+  padding-left: 110px;
+  padding-top: 20px;
+  color: #0275b1;
+`;
+
+export const HR4 = styled.hr`
+  width: 230px;
+  margin-left: 25px;
+  margin-top: 10px;
+`;
+export const UniIcon = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+export const UniIconAndName = styled.button`
+  display: flex;
+  background-color: white;
+  border: white;
+  margin-left: 60px;
+  margin-top: 30px;
+  border-radius: 5px;
+`;
+export const UniName = styled.p`
+  width: 100px;
+`;
+export const SHAWALL = styled.p`
+  color: #0275b1;
+  font-size: 12px;
+  padding-top: 60px;
+  padding-left: 40px;
+`;
+export const FollowedHashtagsDiv = styled.div`
+  width: 290px;
+  height: 265px;
+  border: solid white;
+  margin-left: 150px;
+  margin-top: 50px;
+  background-color: white;
+  border-radius: 5px;
+`;
+export const FollowedHashtags = styled.p`
+  font-size: 12px;
+  padding-left: 25px;
+  padding-top: 20px;
+`;
+export const HR6 = styled.hr`
+  width: 230px;
+  margin-left: 25px;
+  margin-top: 10px;
+`;
+export const Skils = styled.button`
+  width: auto;
+  height: 33px;
+  background-color: #e9f0f8;
+  border: #e9f0f8;
+  border-radius: 2px;
+  margin-left: 30px;
+  margin-top: 10px;
+`;
+export const TrendingDiv = styled.div`
+  width: 290px;
+  height: 300px;
+  border: solid white;
+  margin-left: 150px;
+  margin-top: 50px;
+  background-color: white;
+  border-radius: 5px;
+`;
+export const TrendingArticles = styled.p`
+  font-size: 12px;
+  padding-left: 20px;
+  padding-top: 20px;
+`;
+export const HR7 = styled.hr`
+  width: 230px;
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+export const TrendingImg = styled.img`
+  width: 80px;
+  height: 50px;
+  margin-left: 30px;
+  margin-top: 20px;
+`;
+export const ImgAndInfo = styled.div`
+  display: flex;
+`;
+export const HowIMakeCoolDesigns = styled.p`
+  padding-top: 20px;
+  padding-left: 10px;
+  font-size: 14px;
+`;
+export const Viewers = styled.p`
+  font-size: 10px;
+  padding-left: 10px;
+  padding-top: 5px;
+`;
+export const TwoParagrap = styled.div`
+  display: block;
+`;
+export const IconsDiv = styled.div`
+  margin-left: 700px;
+`;
